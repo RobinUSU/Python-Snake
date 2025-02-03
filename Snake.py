719cf280db417d1d702056eefc9fc475eee914c9
Robin A <Robair1337@gmail.com>
2025-02-03 05:22:55 -0700

    Added a reset and respawn function on death to the game

--- Snake.py
+++ Snake.py
@@ -4,6 +4,7 @@ from random import randrange
 
 # If this import fails, run `pip install freegames` in terminal
 from freegames import square, vector
+from time import sleep
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
@@ -19,6 +20,19 @@ def inside(head):
     "Return True if head inside boundaries."
     return -200 < head.x < 190 and -200 < head.y < 190
 
+# When the snake dies, this new function will restart the game after xs. 
+def restart():
+    global snake, food, aim
+    snake = [vector(10, 0)]
+    food = vector(0, 0)
+    aim = vector(0, -10)
+    
+    sleep(2)
+    clear()
+    
+    move()
+
+
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
@@ -27,29 +41,34 @@ def move():
     if not inside(head) or head in snake:
         square(head.x, head.y, 9, 'red')
         update()
+
+        # Added Restart function, when you die, it waits x seconds to restart the game
+
+        restart()
         return
 
     snake.append(head)
 
     if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
+        food.x = randrange(-12, 12) * 10
+        food.y = randrange(-12, 12) * 10
     else:
         snake.pop(0)
 
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, 'green')
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, 'red')
     update()
-    ontimer(move, 100)
+    ontimer(move, 50)
+
 
 setup(420, 420, 370, 0)
 hideturtle()
-tracer(False)
+tracer(False) # Draws the blocks bit by bit on True, slows down the game dramatically
+
 listen()
 onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
